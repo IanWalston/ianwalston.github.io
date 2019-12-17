755fc36aee79c03e9ad7ba411106eadc4079413d
Ian Walston <ikwalston@comcast.net>
2019-12-17 18:48:08 -0500

    build

---
+++ build/precache-manifest.1f29d2ab18538026b1c2ca4f9381af2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddad3d4296fff1db78f54c38930c8b0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7f3aa9c64fb94603461",
+    "url": "./static/css/main.0881c085.chunk.css"
+  },
+  {
+    "revision": "a8264e52fbf224ac313b",
+    "url": "./static/js/2.8b41c73e.chunk.js"
+  },
+  {
+    "revision": "d7f3aa9c64fb94603461",
+    "url": "./static/js/main.b59ffcd4.chunk.js"
+  },
+  {
+    "revision": "f9517eeb7b895af61f2b",
+    "url": "./static/js/runtime-main.a209cf98.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ianWalston from "./info.json.js";
+import ianWalston from "./info.json";
 
 ReactDOM.render(<App ian={ianWalston} />, document.getElementById('root'));
 
