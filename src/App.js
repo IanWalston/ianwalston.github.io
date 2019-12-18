69e8d08c519a18614a0c3c0dd38e198c2d86d625
Ian Walston <ikwalston@comcast.net>
2019-12-17 23:13:06 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Info from "./components/Info";
 import Projects from "./components/Projects/index";
 import Links from "./components/Links";
-import ExportButton from "./components/ExportButton";
 
 export class App extends Component {
     render() {
@@ -10,18 +9,27 @@ export class App extends Component {
         return (
             <div className="container mt-4 pt-4">
                 <div className="row">
-                    <div className='col-md-8'>
+                    <div className="col-md-8">
                         <Info {...this.props} />
                     </div>
-                    <div className='col-md-4'>
+                    <div className="col-md-4">
                         <Links links={ian.links} />
                     </div>
                 </div>
-                <hr className='my-4'/>
+                <hr className="my-4" />
                 <div className="row">
                     <Projects projects={ian.projects} />
-                    {/* <ExportButton {...this.props} /> */}
                 </div>
+                <hr className="mt-4" />
+                <footer class="page-footer font-small blue">
+                    <div class="footer-copyright text-center py-3">
+                        © 2020
+                        <a href="https://ianwalston.github.io/">
+                            {" "}
+                            ianwalston.github.io
+                        </a>
+                    </div>
+                </footer>
             </div>
         );
     }
