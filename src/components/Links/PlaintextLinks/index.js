10d21966183aa9fb53e51e8d81336e7e2cb9c755
ian <ian@unichord-apps.com>
2020-09-13 12:14:59 -0400

    move string operation out of component function

--- src/components/Links/PlaintextLinks/index.js
+++ src/components/Links/PlaintextLinks/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { TextareaAutosize } from '@material-ui/core'
 
-export default function index(props) {
+function jsonLinksToPlainText (links){
+    return links.map(link => `${link.name}: ${link.displayText ? link.displayText : link.url}`).join('\n')
+}
 
-    const text = props.links.map(link => `${link.name}: ${link.displayText ? link.displayText : link.url}`).join('\n')
-
+export default function index(props) {  
     return <TextareaAutosize
     style={{width:'100%'}}
         fullwidth="true"
         rowsMax={5}
-        defaultValue={text}
+        defaultValue={jsonLinksToPlainText(props.links)}
     />;
 }
